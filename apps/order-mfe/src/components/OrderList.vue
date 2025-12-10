bcf9c6b70484928bf19fa57ac520d27af66df5a4
disara7 <129250994+disara7@users.noreply.github.com>
2025-12-10 22:14:35 +0530

    Reapply "created order service"
    This reverts commit 10032aab9d7c804e0b5ed90ae405d8ca6b601c20.

---
+++ apps/order-mfe/src/components/OrderList.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h3>Orders</h3>
+    <ul>
+      <li v-for="order in orders" :key="order.id">
+        {{ order.item }} - ${{ order.amount }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+const API_URL = "http://localhost:4002/api/orders";
+
+export default {
+  data() {
+    return { orders: [] };
+  },
+  mounted() {
+    axios.get(API_URL).then((res) => (this.orders = res.data));
+  },
+};
+</script>
---
+++ services/order-service/src/routes/order.routes.js
@@ -0,0 +1,15 @@
+import express from "express";
+import db from "../db.js";
+
+const router = express.Router();
+
+router.get("/orders", async (req, res) => {
+  try {
+    const [rows] = await db.query("SELECT * FROM orders");
+    res.json(rows);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+export default router;
