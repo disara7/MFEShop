0d1e71b78d1b894c58a894a62448efb9d872a5e8
disara7 <129250994+disara7@users.noreply.github.com>
2025-12-10 22:13:36 +0530

    created order service

---
+++ apps/order-mfe/src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <div style="padding:20px">
+    <h2>📦 Order Microfrontend</h2>
+    <OrderList />
+  </div>
+</template>
+
+<script>
+import OrderList from "./components/OrderList.vue";
+export default { components: { OrderList } };
+</script>
